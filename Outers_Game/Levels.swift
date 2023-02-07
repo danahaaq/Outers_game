20fe5e88f093f174358f3bcf7b427215420cf7a0
Ghadahss <Ghadah.s.alzahrani@gmail.com>
2023-02-07 17:18:45 +0300

    add levels file

---
+++ Outers_Game/Levels.swift
@@ -0,0 +1,20 @@
+//
+//  Levels.swift
+//  Outers_Game
+//
+//  Created by Ghadah on 16/07/1444 AH.
+//
+
+import SwiftUI
+
+struct Levels: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Levels_Previews: PreviewProvider {
+    static var previews: some View {
+        Levels()
+    }
+}
