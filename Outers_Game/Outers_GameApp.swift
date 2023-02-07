e98a6e23d15a02160fd8393b48ac920cb339fc2b
Aldanah Alqabbani <danaalialq@gmail.com>
2023-02-07 16:34:05 +0300

    Initial Commit

---
+++ Outers_Game/Outers_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outers_GameApp.swift
+//  Outers_Game
+//
+//  Created by Aldanah Alqbbani on 16/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Outers_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
