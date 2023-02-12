4524d10a76d90a79cc05fdf43d0ad1f0904ef380
Aldanah Alqabbani <danaalialq@gmail.com>
2023-02-12 14:00:14 +0300

    Add multiple changes: - add coaching - scenario and the play timer

--- Outers_Game/ContentView.swift
+++ Outers_Game/ContentView.swift
@@ -6,21 +6,67 @@
 //
 
 import SwiftUI
+import RealityKit
+import ARKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIView {
+        let arView = ARView(frame: .zero)
+        let session = arView.session
+        
+        let coachingOverly = ARCoachingOverlayView()
+        // this is an intger bit mask that determines how the reciever resized itself.
+        coachingOverly.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        coachingOverly.goal = .horizontalPlane
+        coachingOverly.session = session
+        // Allow the coachingOverly to start the begain at first
+        coachingOverly.setActive(true, animated: true)
+        arView.addSubview(coachingOverly)
+        // Load the "Opject" scene from the "dumu" Reality File
+        let anchor = try! Dumu.loadScene()
+        // Add the Opject anchor to the scene
+        arView.scene.anchors.append(anchor)
+        
+        return arView
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        // Optional: update the view if necessary
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+
+struct ContentView : View {
+    @State var one1 = false
+    
+    var body: some View {
+        ZStack{
+            
+            if one1 == false{
+                ARViewContainer().edgesIgnoringSafeArea(.all)
+                
+            }else if one1 == true{
+                
+                Scinario()
+                
+            }
+        }.onAppear{
+            DispatchQueue.main.asyncAfter(deadline: .now()+5.0){
+                withAnimation{
+                    one1 = true
+                }
+            }
+            
+        }
+        
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
---
+++ Outers_Game/Scinario.swift
@@ -0,0 +1,139 @@
+//
+//  Scinario.swift
+//  Outers_Game
+//
+//  Created by Aldanah Alqbbani on 18/07/1444 AH.
+//
+
+import Foundation
+import SwiftUI
+import RealityKit
+
+struct ARViewContainer2: UIViewRepresentable {
+   
+    func makeUIView(context: Context) -> UIView {
+        let arView = ARView(frame: .zero)
+        
+     return arView
+    }
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+//         Optional: update the view if necessary
+    }
+}
+struct Scinario : View {
+    @State var ispressed2 : Bool = false
+    @State var countdownTimer = 3
+    @State var timerRunning = true
+    
+    //    The publish(every:tolerance:on:in:options:) operator creates a Timer.TimerPublisher, which is a ConnectablePublisher. As a result, subscribers don’t receive any values until after a call to connect(). For convenience when working with a single subscriber, the autoconnect() operator performs the connect() call when attached to by the subscriber.
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack{
+            ARViewContainer2()
+                .ignoresSafeArea()
+            if ispressed2 == false{
+                ZStack{
+                    Rectangle()
+                        .frame(width: 318,height: 340)
+                        .foregroundColor(.white)
+                        .cornerRadius(30)
+                        .opacity(0.8)
+                    
+                    VStack(alignment: .leading,spacing:-170){
+                        Text("Tom’s \n needs his  car , home and office keys, find \n them!")
+                            .multilineTextAlignment(.center)
+                            .frame(width: 318,height: 329)
+                            .foregroundColor(Color(red: 0.682, green: 0.303, blue: 0.162))
+                        
+                        //                         New added font
+                            .font(Font.custom("RussoOne-Regular", size: 20))
+                        
+                        Image("dad")
+                            .resizable()
+                            .frame(width: 142, height: 224)
+                    }
+                }
+                ZStack(){
+                    Button{
+                        ispressed2 = true
+                    }label: {
+                        Rectangle()
+                            .frame(width: 318,height: 53)
+                            .foregroundColor(.white)
+                            .cornerRadius(30)
+                            .opacity(0.5)
+                            .overlay(){
+                                Text("Start")
+                                    .foregroundColor(Color(red: 0.345, green: 0.59, blue: 0.878))
+                                    .font(Font.custom("RussoOne-Regular", size: 24))
+                            }
+                    }
+                }.padding(.top,576)
+            }
+            else {
+                ZStack{
+                    VStack {
+                        if countdownTimer > 0{
+                            Text("\(countdownTimer)")
+                                .padding()
+                                .onReceive(timer) { _ in
+                                    if countdownTimer > 0 && timerRunning {
+                                        countdownTimer -= 1
+                                    } else {
+                                        //
+                                        timerRunning = false
+                                    }
+                                }
+                                .font(Font.custom("RussoOne-Regular", size: 128))
+                                .foregroundColor(.white)
+                            //                            For the timer font
+                                .glowBorder(color: Color(red: 0.345, green: 0.59, blue: 0.878), lineWidth: 5)
+                        }
+                        else{
+                            Text("GO")
+                                .padding()
+                                .font(Font.custom("RussoOne-Regular", size: 128))
+                                .foregroundColor(.white)
+                            
+                            //                            For the timer font
+                                .glowBorder(color: Color(red: 0.345, green: 0.59, blue: 0.878), lineWidth: 5)
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+struct Scinario_Previews : PreviewProvider {
+    static var previews: some View {
+        Scinario()
+    }
+}
+
+
+// Add Thick border to a the words itself
+struct GlowBorder: ViewModifier {
+    var color: Color
+    var lineWidth: Int
+    
+    func body(content: Content) -> some View {
+        applyShadow(content: AnyView(content), lineWidth: lineWidth)
+    }
+    
+    func applyShadow(content: AnyView, lineWidth: Int) -> AnyView {
+        if lineWidth == 0 {
+            return content
+        } else {
+            return applyShadow(content: AnyView(content.shadow(color: color, radius: 1)), lineWidth: lineWidth - 1)
+        }
+    }
+}
+
+extension View {
+    func glowBorder(color: Color, lineWidth: Int) -> some View {
+        self.modifier(GlowBorder(color: color, lineWidth: lineWidth))
+    }
+}
